16cda74ff26e0c7b40bcca73550cca0c4d5108a6
yutokyokutyo <k.yuto.cre@gmail.com>
2014-10-29 23:57:24 +0900

    arrayについて勉強しますた

---
+++ study/array.rb
@@ -0,0 +1,9 @@
+array = ["Bear", "Benford", "Egain"]
+p array.map{|name| "G. #{name}" }
+
+hash = {
+"Baxter" => "Stephen", "Stross" => "Charles", "Reynolds" => "Alastar"
+}
+hash.each do |key, value|
+  puts "#{value} #{Key}"
+end
